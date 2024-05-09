d07b884871b9ff929f0c9f02811ccb4e1c0b0109
tduncan14 <nbatre1@yahoo.com>
2024-05-09 08:17:59 -0700

    keep moving forward

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  content: ['./*.html'],
+  theme: {
+    extend: {
+      cyan: 'hsl(180,66%,49%)',
+      cyanLight: 'hsl(180,66%,69%)',
+      darkViolet: 'hsl(257,27%,26%)',
+      red: 'hsl(0,87%,67%)',
+      grayishViolet: 'hsl(257,7%,63%)',
+      veryDarkBlue: 'hsl(260,8%,14%)',
+      veryDarkViolet: 'hsl(260,8%,14%)'
+    },
+    fontFamily: {
+      sans: ['Poppins', 'sans-serif']
+    },
+    spacing: {
+      180: '32rem'
+    }
+  },
+  plugins: [],
+}
