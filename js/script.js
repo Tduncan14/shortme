e9bc89da684b04f8d1d9f3b2d487b31818bc519e
tduncan14 <nbatre1@yahoo.com>
2024-05-17 13:20:37 -0700

    keep moving forward

--- js/script.js
+++ js/script.js
@@ -2,6 +2,25 @@
 const input = document.getElementById('link-input');
 const linkForm = document.getElementById('link-form');
 const errMsg = document.getElementById('error');
+const btn = document.getElementById('btn');
+const menu = document.getElementById('menu');
+
+
+
+
+
+btn.addEventListener('click', navToggle);
+
+
+
+// Toggle Mobile Menu
+
+function navToggle() {
+    btn.classList.toggle('open');
+    menu.classList.toggle('flex');
+    menu.classList.toggle('hidden');
+}
+
 
 
 linkForm.addEventListener('submit', formSubmit);
